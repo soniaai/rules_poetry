77f94c6ab5dcd41b74f26bef903744d7a71a36ca
Nathan Howell <nathan@sonia.com>
2019-11-05 13:05:01 -0800

    Update tests

--- test/test_sys_marker.py
+++ test/test_sys_marker.py
@@ -3,13 +3,12 @@ import unittest
 class TestSysMarker(unittest.TestCase):
     def test_markers(self):
         def fail() -> None:
-            # this is a dependency of ipython but only on Windows
+            # this is a dependency for sunos4, should not be present on other platforms
             from colorama import init
             init()
-        raise ValueError()
 
         self.assertRaises(ImportError, fail)
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
